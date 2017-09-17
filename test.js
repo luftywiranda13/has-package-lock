78c6f7a5d5e1fc155dccacd67ee00639ef61b654
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-18 02:31:27 +0700

    test: 100% code coverage

--- test.js
+++ test.js
@@ -6,4 +6,5 @@ test('output', () => {
   expect(hasPackageLock('fixtures/bar')).toBe(true);
   expect(hasPackageLock('fixtures/foo')).toBe(false);
   expect(hasPackageLock('fixtures')).toBe(false);
+  expect(hasPackageLock()).toBe(false);
 });
