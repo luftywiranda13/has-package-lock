e387ee3097d3a8dac0be354fe843fa6f0adfa4b6
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-18 02:31:01 +0700

    chore: meta tweaks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 module.exports = cwd => fs.existsSync(path.resolve(cwd || process.cwd(), 'package-lock.json'));
